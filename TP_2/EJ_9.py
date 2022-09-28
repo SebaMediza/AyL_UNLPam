aaa1dad4f09833144e443f4d393afeb1df817f77
Sebastian Mediza <sebamediza@hotmail.com>
2022-09-28 13:57:47 -0300

    Cuarta_Iteracion

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -1,12 +1,11 @@
 import json
 
 
-def turing_machine(cadena: str, rules: str) -> bool:
-
-        return True
+def turing_machine(cadena: str, regla: str) -> bool:
+    json.load(regla)
 
 
 if __name__ == '__main__':
     string = '00010010101010'
-    reglas = r"C:\Users\sebam\Documents\Carreras\Ingenieria en sistemas\2° Año\2° Semestre\Autómatas y Lenguajes\EJ_1.jff"
+    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\data.json'
     turing_machine(string, reglas)
