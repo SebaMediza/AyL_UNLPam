40465ea28e35c2119e81fd7252419d0d49f3eae4
Seba Mediza <sebamediza@hotmail.com>
2022-09-29 08:17:23 -0300

    Cosas

--- Test.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    with open('TP_1/MOCK_DATA.csv', 'r', encoding="Latin-1") as my_list:
-        for line in my_list:
-            print(line, end="")
---
+++ Tests/Test.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    with open('TP_1/MOCK_DATA.csv', 'r', encoding="Latin-1") as my_list:
+        for line in my_list:
+            print(line, end="")
