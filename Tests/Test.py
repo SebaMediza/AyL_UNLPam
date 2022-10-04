acaad10b43d98991e6d60df64f67e4f910de63e6
Seba Mediza <sebamediza@hotmail.com>
2022-10-04 15:27:36 -0300

    Cora

--- Tests/Test.py
+++ Tests/Test.py
@@ -1,4 +1,18 @@
+from turtle import *
+
+
+def heart() -> None:
+    color('red')
+    begin_fill()
+    pensize(3)
+    left(50)
+    forward(133)
+    circle(50, 200)
+    right(140)
+    circle(50, 200)
+    forward(133)
+    end_fill()
+
+
 if __name__ == '__main__':
-    with open('TP_1/MOCK_DATA.csv', 'r', encoding="Latin-1") as my_list:
-        for line in my_list:
-            print(line, end="")
+    heart()
