fe2c3971ba107dfec606345877364614ef89d84e
Seba Mediza <sebamediza@hotmail.com>
2022-10-07 16:07:41 -0300

    La idea esta, solo falta la ejecucion

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -1,23 +1,28 @@
-import xml.dom.minidom
+import json
 
 
-def turing_machine(cadena: str, regla: str) -> None:
-    rules = xml.dom.minidom.parse(regla)
-    bloques = rules.getElementsByTagName('transition')
-    transicionFrom = rules.getElementsByTagName('from')
-    transicionTo = rules.getElementsByTagName('to')
-    readCharacter = rules.getElementsByTagName('read')
-    writeCharacter = rules.getElementsByTagName('write')
-    movement = rules.getElementsByTagName('move')
-    final = rules.getElementByTagName('final')
-    # print("%d block" % bloques.length)
-    print(transicionTo)
-    print(transicionFrom)
-    # while transicionTo != final:
+def turing_machine(cadena: str, regla: str) -> bool:
+    with open(regla, 'r') as turing:
+        data = json.load(turing)
+        q = data['Q']
+        delta = data['Delta']
+        first = data['q0']
+        end = data['F']
+        now = first
+        while now != end:
+            for i in cadena:
+                if i in first:
+                    print(i)
+                now = delta['q1']
+                if i in now:
+                    print(i)
+                if i == '#':
+                    now = end
 
+    return True
 
 
 if __name__ == '__main__':
-    string = '0'
-    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_1.jff'
-    turing_machine(string, reglas)
+    string = '00001111#'
+    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\data.json'
+    print(turing_machine(string, reglas))
