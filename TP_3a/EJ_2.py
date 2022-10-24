36dd65afc331928280d57ad8b0c4b1427c47131f
Seba Mediza <sebamediza@hotmail.com>
2022-10-24 20:33:44 -0300

    Lo entendí, por fin

--- TP_3a/EJ_2.py
+++ TP_3a/EJ_2.py
@@ -1,23 +1,22 @@
-class ParseA(object):
-    def cadena(self: str):
-        print(self)
+import xml.etree.ElementTree as ET
 
 
-class ParseB(object):
-    def cadena(self: str):
-        print(self)
-
-
-class ParseC(object):
-    def cadena(self: str):
-        print(self)
-
-
-class ParseD(object):
-    def cadena(self: str):
-        print(self)
+def parse_a(table: str, string: str):
+    print(string)
+    with open(table, 'r') as rules:
+        ll1 = ET.parse(rules)
+        myroot = ll1.getroot()
+        for x in myroot.findall('production'):
+            right = x.find('right').text
+            if string[0] == right[0]:
+                print('Elemento del XML: ', right[0])
+                print('Elemmento de la Cadena: ', string[0])
+                string = string[1:]
+                print(string)
+                print(True)
 
 
 if __name__ == '__main__':
-    p = ParseA
-    p.cadena('')
+    file = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_3a\1a.jff'
+    cadena = '(a+a)$'
+    parse_a(file, cadena)
