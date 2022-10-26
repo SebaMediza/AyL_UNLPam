5011793e9252940571e02ab29ca12753cd222148
Sebastian Mediza <sebamediza@hotmail.com>
2022-10-25 22:38:16 -0300

    cambios finales

--- TP_3a/parser_a.py
+++ TP_3a/parser_a.py
@@ -2,53 +2,57 @@ import xml.etree.ElementTree as ET
 
 
 class ParserA(object):
-    def evaluate(self: str, cadena: str):
-        def macht(string: str, stack: str, resultado: str):
-            while string[0] == stack[0]:
-                string = string[1:]
-                stack = stack[1:]
-                if string[0] == '$':
-                    print('La cadena pertenece al lenguaje')
-                    exit(0)
-            if stack[0] == 'S':
-                S(string, stack, resultado)
-            if stack[0] == 'F':
-                F(string, stack, resultado)
 
-        def S(string: str, stack: str, resultado: str):
-            for x in myroot.findall('production'):
-                right = x.find('right').text
-                left = x.find('left').text
-                if stack[0] == left[0] and string[0] == right[0]:
-                    stack = right + stack[1:]
-                    break
-                if stack[0] == 'S' and string[0] == 'a':
-                    stack = 'F' + stack[1:]
-                    break
-            macht(string, stack, resultado)
-            return resultado
+    def __init__(self):
+        self.string = None
+        self.stack = None
+        self.file = None
+        self.myroot = None
 
-        def F(string: str, stack: str, resultado: str):
-            for x in myroot.findall('production'):
-                right = x.find('right').text
-                left = x.find('left').text
-                if stack[0] == left[0] and string[0] == right[0]:
-                    stack = 'a' + stack[1:]
-                    break
-            macht(string, stack, resultado)
-            return resultado
-
-        with open(self, 'r') as rules:
+    def evaluate(self, cadena, file):
+        self.string = cadena
+        self.file = file
+        with open(self.file, 'r') as rules:
             ll1 = ET.parse(rules)
-            myroot = ll1.getroot()
-            pila = 'S'
-            result = ''
-            result = S(cadena, pila, result)
-            return result
+            self.myroot = ll1.getroot()
+            self.stack = 'S'
+            self.S(self.myroot)
+        if self.string[0] == '$':
+            return True
+
+    def macht(self):
+        while self.string[0] == self.stack[0]:
+            self.string = self.string[1:]
+            self.stack = self.stack[1:]
+        if self.stack[0] == 'S':
+            self.S(self.myroot)
+        if self.stack[0] == 'F':
+            self.F(self.myroot)
+
+    def S(self, myroot):
+        for x in myroot.findall('production'):
+            right = x.find('right').text
+            left = x.find('left').text
+            if self.stack[0] == left[0] and self.string[0] == right[0]:
+                self.stack = right + self.stack[1:]
+                break
+            if self.stack[0] == 'S' and self.string[0] == 'a':
+                self.stack = 'F' + self.stack[1:]
+                break
+        self.macht()
+
+    def F(self, myroot):
+        for x in myroot.findall('production'):
+            right = x.find('right').text
+            left = x.find('left').text
+            if self.stack[0] == left[0] and self.string[0] == right[0]:
+                self.stack = 'a' + self.stack[1:]
+                break
+        self.macht()
 
 
 if __name__ == '__main__':
-    file = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_3a\1a.jff'
-    chain = 'aa'
-    p = ParserA
-    p.evaluate(file, chain + '$')
+    p = ParserA()
+    word = 'a'
+    table = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_3a\parser_a_tabla_ll1.jff'
+    print(p.evaluate(word + '$', table))
