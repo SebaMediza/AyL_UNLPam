ffc91a75c397e56f0fe8a3aa071232418eddb82d
Sebastian Mediza <sebamediza@hotmail.com>
2022-10-28 12:15:32 -0300

    Ta todo listo

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -6,16 +6,25 @@ def turing_machine(cadena: str, regla: str):
         turing = ET.parse(file)
         myroot = turing.getroot()
         for y in myroot:
+            for t in y.findall('block'):
+                final = t.find('final')
+                if final is not None:
+                    print(cadena, t.get('id'))
+
             for x in y.findall('transition'):
-                desde = x.find('from').text
+                fromm = x.find('from').text
                 to = x.find('to').text
                 read = x.find('read').text
                 write = x.find('write').text
-                move = x.find('move').text
-                print(desde, to, read, write, move)
+
+                if cadena[0] == read:
+                    cadena = write + cadena[1:]
+                    to = fromm
+                    if to == t.get('id'):
+                        exit(0)
 
 
 if __name__ == '__main__':
     string = '00001111#'
-    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_3.jff'
+    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_1.jff'
     turing_machine(string, reglas)
