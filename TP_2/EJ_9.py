7d58447919ffa2cefb306710352d65ee38f2c74e
Seba Mediza <sebamediza@hotmail.com>
2022-10-31 15:35:53 -0300

    .

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -5,22 +5,21 @@ def turing_machine(cadena: str, regla: str):
     with open(regla, 'r') as file:
         turing = ET.parse(file)
         myroot = turing.getroot()
+        estadoActual = '0'
         for y in myroot:
             for t in y.findall('block'):
-                final = t.find('final')
-                if final is not None:
-                    print(cadena, t.get('id'))
+                if t.find('final') is not None:
+                    final = t.get('id')
 
             for x in y.findall('transition'):
-                fromm = x.find('from').text
-                to = x.find('to').text
                 read = x.find('read').text
                 write = x.find('write').text
-
-                if cadena[0] == read:
+                if cadena[0] == read and estadoActual == '0':
+                    estadoActual = x.find('from').text
+                    proximoEstado = x.find('to').text
                     cadena = write + cadena[1:]
-                    to = fromm
-                    if to == t.get('id'):
+                    proximoEstado = estadoActual
+                    if proximoEstado == final:
                         exit(0)
 
 
