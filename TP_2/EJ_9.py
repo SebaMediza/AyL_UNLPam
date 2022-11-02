bb03d4b6d233a5dcf71164598dcfaf6eddcd956b
Sebastian Mediza <sebamediza@hotmail.com>
2022-11-01 23:17:42 -0300

    Algo

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -5,25 +5,27 @@ def turing_machine(cadena: str, regla: str):
     with open(regla, 'r') as file:
         turing = ET.parse(file)
         myroot = turing.getroot()
-        estadoActual = '0'
+        index = 0
         for y in myroot:
+            for h in y.findall('block'):
+                if h.find('initial') is not None:
+                    inicial = h.get('id')
             for t in y.findall('block'):
                 if t.find('final') is not None:
                     final = t.get('id')
-
+        while cadena[index] != '#':
             for x in y.findall('transition'):
                 read = x.find('read').text
                 write = x.find('write').text
-                if cadena[0] == read and estadoActual == '0':
-                    estadoActual = x.find('from').text
-                    proximoEstado = x.find('to').text
+                estadoActual = x.find('from').text
+                if cadena[index] == read and estadoActual == inicial:
                     cadena = write + cadena[1:]
-                    proximoEstado = estadoActual
-                    if proximoEstado == final:
-                        exit(0)
+                    proximoEstado = x.find('to').text
+                    inicial = proximoEstado
+                    index = index + 1
 
 
 if __name__ == '__main__':
-    string = '00001111#'
-    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_1.jff'
+    string = '1 '
+    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_3.jff'
     turing_machine(string, reglas)
