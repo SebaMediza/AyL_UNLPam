36535a77dd2887dc17419d1d502d95645083e58d
Sebastian Mediza <sebamediza@hotmail.com>
2022-11-04 14:42:20 -0300

    Último

---
+++ TP_4/EJ_9.py
@@ -0,0 +1,15 @@
+class NFA(object):
+    def __init__(self, file=None):
+        super(NFA, self).__init__()
+        pass
+
+    def load_from_file(self, filename):
+        pass
+
+    def run(self, word):
+        # return true | false
+
+
+if __name__ == '__main__':
+    p = NFA()
+    print(p)
