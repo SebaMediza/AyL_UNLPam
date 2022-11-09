dc8e04ba19943b297e795c50cf18d57f50703ea9
Sebastian Mediza <sebamediza@hotmail.com>
2022-11-09 12:03:05 -0300

    Nop, sigue roto

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -24,22 +24,31 @@ def turing_machine(cadena: str, regla: str):
                 read = x.find('read').text
                 write = x.find('write').text
                 fromm = x.find('from').text
+                direction = x.find('move').text
                 if cadena == '':
+                    cadena = None
+                if cadena is None and read is None:
                     estadoActual = x.find('to').text
-                    break
-                elif cadena[0] not in lenguaje:
-                    return 'La cadena no pertenece al leguaje reconocido por la maquina', False
-                elif cadena[0] == read and estadoActual == fromm:
-                    # print(write)
-                    cadena = cadena[1:]
-                    result = result[0:indice] + write + result[indice + 1:]
-                    indice = indice + 1
-                    estadoActual = x.find('to').text
-    return result, True
+                if cadena is not None:
+                    if cadena[0] not in lenguaje:
+                        return 'La cadena no pertenece al leguaje reconocido por la maquina', False
+                    if cadena[0] == read and estadoActual == fromm and direction == 'R':
+                        cadena = cadena[1:]
+                        result = result[0:indice] + write + result[indice + 1:]
+                        indice = indice + 1
+                        estadoActual = x.find('to').text
+                        break
+                    if cadena[0] == read and estadoActual == fromm and direction == 'L':
+                        cadena = cadena[1:]
+                        result = result[indice + 1:] + write + result[0:indice]
+                        indice = indice + 1
+                        estadoActual = x.find('to').text
+                        break
+        return estadoActual == final, result
 
 
 if __name__ == '__main__':
     string = '0110'
-    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_3.jff'
+    reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_5.jff'
     print('Cadena: ', string)
     print('Cadena devuelta por la maquina: ', turing_machine(string, reglas))
