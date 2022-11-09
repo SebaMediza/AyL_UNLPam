1bbc593241b5797277017b73b6786b38dd2a469b
Seba Mediza <sebamediza@hotmail.com>
2022-11-09 08:16:57 -0300

    Lo pude hacer

--- TP_2/EJ_9.py
+++ TP_2/EJ_9.py
@@ -5,27 +5,35 @@ def turing_machine(cadena: str, regla: str):
     with open(regla, 'r') as file:
         turing = ET.parse(file)
         myroot = turing.getroot()
-        index = 0
+        result = ''
+        indice = 0
         for y in myroot:
             for h in y.findall('block'):
                 if h.find('initial') is not None:
                     inicial = h.get('id')
+                    estadoActual = inicial
             for t in y.findall('block'):
                 if t.find('final') is not None:
                     final = t.get('id')
-        while cadena[index] != '#':
+        while estadoActual != final:
             for x in y.findall('transition'):
                 read = x.find('read').text
                 write = x.find('write').text
-                estadoActual = x.find('from').text
-                if cadena[index] == read and estadoActual == inicial:
-                    cadena = write + cadena[1:]
-                    proximoEstado = x.find('to').text
-                    inicial = proximoEstado
-                    index = index + 1
+                fromm = x.find('from').text
+                if cadena == '':
+                    estadoActual = x.find('to').text
+                    break
+                if cadena[0] == read and estadoActual == fromm:
+                    # print(write)
+                    cadena = cadena[1:]
+                    result = result[0:indice] + write + result[indice + 1:]
+                    indice = indice + 1
+                    estadoActual = x.find('to').text
+    return result
 
 
 if __name__ == '__main__':
-    string = '1 '
+    string = '010110'
     reglas = r'C:\Users\sebam\Documents\Programacion\AyL_UNLPam\TP_2\EJ_3.jff'
-    turing_machine(string, reglas)
+    print('Cadena: ', string)
+    print('Cadena devuelta por la maquina: ', turing_machine(string, reglas))
