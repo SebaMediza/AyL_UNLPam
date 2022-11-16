72db111063869fd2e5455273f140cb959d38d358
Sebastian Mediza <sebamediza@hotmail.com>
2022-11-16 19:13:02 -0300

    Creeria que ahi anda

--- TP_4/NFA.py
+++ TP_4/NFA.py
@@ -9,7 +9,7 @@ class NFA(object):
     fromm = ''
     actual = ''
     lenguaje = []
-    newStates = []
+    newstates = []
     AFND = False
     result = None
     archivo = ''
@@ -26,6 +26,10 @@ class NFA(object):
             test = ET.parse(anfd)
             root = test.getroot()
             for y in root:
+                for g in y.findall('transition'):
+                    if g.find('read').text is None:
+                        self.splitparse(self.archivo)
+                        break
                 for x in y.findall('state'):
                     if x.find('initial') is not None:
                         self.initial = x.get('id')
@@ -37,16 +41,9 @@ class NFA(object):
                 for t in y.findall('transition'):
                     if t.find('read').text not in self.lenguaje:
                         self.lenguaje.append(t.find('read').text)
-                for g in y.findall('transition'):
-                    if g.find('read').text is None:
-                        self.AFND = True
         print('El lenguaje reconocido es:', self.lenguaje)
         print('Cadena a reconocer:', word)
         self.cadena = word
-        if self.AFND is False:
-            self.parse(self.archivo)
-        else:
-            self.split(self.archivo)
         return self.result
 
     def parse(self, filename):
@@ -64,43 +61,17 @@ class NFA(object):
                                 self.actual = x.find('to').text
             self.result = self.actual == self.final
 
-    def split(self, filename):
-        with open(filename, 'r') as AFND:
-            machine = ET.parse(AFND)
-            root = machine.getroot()
-            for y in root:
-                for x in y.findall('transition'):
-                    if x.find('read').text is None:
-                        self.newStates.append(x.find('to').text)
-                        self.add(filename)
-                        break
-        self.splitParse(filename)
-
-    def add(self, filename):
-        with open(filename, 'r') as AFND:
-            machine = ET.parse(AFND)
-            root = machine.getroot()
-            for y in root:
-                for x in y.findall('transition'):
-                    if self.newStates[0] == x.find('from').text:
-                        self.newStates.append(x.find('to').text)
-
-    def splitParse(self, filename):
-        with open(filename, 'r') as AFND:
-            machine = ET.parse(AFND)
-            root = machine.getroot()
+    def splitparse(self, filename):
+        with open(filename, 'r') as anfd:
+            test = ET.parse(anfd)
+            root = test.getroot()
             while self.cadena != '':
                 for y in root:
                     for x in y.findall('transition'):
-                        self.read = x.find('read').text
-                        self.fromm = x.find('from').text
-                        if self.cadena[0] == self.read and self.fromm in self.newStates and self.actual == self.fromm:
-                            self.cadena = self.cadena[1:]
-                            self.actual = x.find('to').text
-                            break
-            self.result = self.actual == self.final
+                        if x.find('read') is None:
+                            self.newstates.append(x.find('to').text)
 
 
 if __name__ == '__main__':
-    p = NFA('./AFND_epsilon.jff')
+    p = NFA('./AFND_epsilon_even_0_or_1.jff')
     print('Resultado:', p.run('1111'))
