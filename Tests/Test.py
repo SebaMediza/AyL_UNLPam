5a63bb0ec7d8d8065e03ebf055be1e6db470726e
Seba Mediza <sebamediza@hotmail.com>
2022-11-17 15:01:39 -0300

    PowerSet

--- Tests/Test.py
+++ Tests/Test.py
@@ -1,19 +1,17 @@
-from turtle import *
+from itertools import chain, combinations
 
 
-def heart():
-    teso = [
-        [
-            'hello'
-        ],
-        [
-            'hello2'
-        ]
-    ]
-    for t in teso:
-        for h in t:
-            print(h)
+# Mejor opción:
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+def main():
+    l1 = ["p", "q", "r", "s"]
+
+    print([i for i in powerset(l1)], "\n")
 
 
 if __name__ == '__main__':
-    heart()
+    main()
